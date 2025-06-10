db21d0b609c231236462598ccabc3190d7392dff
MuhammadKashifAnsari123 <muhammadkashifa82@gmail.com>
2025-06-11 01:26:30 +0500

    add user and video model

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { app } from "./app.js";
 dotenv.config({
     path:"./env"
 })
+app.get("/",(req, res) => {
+    res.send("hello server is 8000")
+})
 connectdDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
@@ -16,6 +19,8 @@ connectdDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!!",err)
 })
+
+//white list
 
 /*
 import express from "express";
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,11 +13,11 @@ class ApiError extends Error{
             this.errors = errors
 
             if(statck){
-                this.stack
+                this.stack = statck
             }else{
                 Error.captureStackTrace(this, this.constructor)
             }
         }
 }
 
-export default ApiError
+export {ApiError}
