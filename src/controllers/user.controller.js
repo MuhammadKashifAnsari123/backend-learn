66660039e34cb710003c90ee9a4d28f54493a9fb
MuhammadKashifAnsari123 <muhammadkashifa82@gmail.com>
2025-07-07 19:06:48 +0500

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -175,7 +175,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request");
   }
  try {
@@ -187,7 +187,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
    if (!user) {
      throw new ApiError(401, "Invalid Refresh Token");
    }
-   if (incomingRefreshToken !== user?.refreshToken) {
+   if (!incomingRefreshToken !== user?.refreshToken) {
      throw new ApiError(401, "Refresh token is expired or used");
    }
    const options = {
